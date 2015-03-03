c7cd2afc23294526c53eab7dd7090c1d189f79b2
Derek George <derek68038@gmail.com>
2015-03-03 14:57:31 -0600

    Completed exercises in first three problems. Didn't answer the questions.

---
+++ JS02/global.js
@@ -0,0 +1,13 @@
+//JS Atomic 02
+
+//greeting
+alert("Hi, my name is Derek!");
+
+//console.log statements:
+console.log("My favorite breakfast is pancakes and bacon.");
+console.log("I live with two crazy cats.");
+console.log("My favorite place to eat lot's of food is at home.");
+console.log("I maybe take one nap per week.");
+console.log("I would like to take many more naps per week.");
+
+alert("Goodbye!");
---
+++ JS03/global.js
@@ -0,0 +1,11 @@
+var name = "Derek";
+var age = 34;
+var hairColor = "brown";
+
+alert("Hi my name is " + name + ".");
+alert("I'm " + age + " years old.");
+alert("My hair color is " + hairColor + ".");
+
+age = 35;
+
+alert("I'm " + age + " years old.");
